3c52557024cd73153a805822d7335dd675e5eb68
Mark Prettyman <markiesmyname@Marks-iMac.local>
2023-04-23 11:50:12 -0700

    array fund method loop

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find((entry) => entry.result === "W");
+    if (winningRecord) {
+        return winningRecord.year;
+    }   else {
+        return undefined;
+    }
+    }
